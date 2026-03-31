021868e8c17eac10105b0fff2f8fe98fb1112034
Rubench974 <rubenskieken@gmail.com>
2026-03-31 16:16:58 +0200

    ajout de la page mot de passe oublié et liaison du router

--- FrontEnd/app_twin-care/src/router/index.js
+++ FrontEnd/app_twin-care/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import PageConnexion from '../pages/PageConnexion.vue'
+import PageMotDePasseOublie from '../pages/PageMotDePasseOublie.vue'
 import PageDocuments from '../pages/PageDocuments.vue'
 import PageParcours from '../pages/PageParcours.vue'
 import PageChatbot from '../pages/PageChatbot.vue'
@@ -8,6 +9,7 @@ import PageEnvironnement from '../pages/PageEnvironnement.vue'
 
 const routes = [
   { path: '/', component: PageConnexion },
+  { path: '/mot-de-passe-oublie', component: PageMotDePasseOublie },
   { path: '/documents', component: PageDocuments },
   { path: '/parcours', component: PageParcours },
   { path: '/chatbot', component: PageChatbot },
