605b9149b3bca9882a25ba4d34b86e5b0b10c351
Rubench974 <rubenskieken@gmail.com>
2026-03-31 15:18:19 +0200

    nettoyage du template (suppression TypeScript) et configuration Vite/Vuetify

--- FrontEnd/app_twin-care/vite.config.js
+++ FrontEnd/app_twin-care/vite.config.js
@@ -4,18 +4,13 @@ import Fonts from 'unplugin-fonts/vite'
 import { defineConfig } from 'vite'
 import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     Vue({
       template: { transformAssetUrls },
     }),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
     Vuetify({
       autoImport: true,
-      styles: {
-        configFile: 'src/styles/settings.scss',
-      },
     }),
     Fonts({
       fontsource: {
@@ -34,15 +29,7 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('src', import.meta.url)),
     },
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
-    ],
+    extensions: ['.js', '.json', '.jsx', '.mjs', '.vue'],
   },
   server: {
     port: 3000,
